15331895d080e447ca92d76dd43bc9a077bb384c
Nocterum <master.of.colours@yandex.ru>
2023-05-25 19:30:55 +0300

    change users => user

--- models.js
+++ models.js
@@ -1,7 +1,7 @@
 const sequelize = require('./db');
 const {DataTypes} = require('sequelize');
 
-const UserModel = sequelize.define( 'users', {
+const UserModel = sequelize.define( 'user', {
     id: {type: DataTypes.INTEGER, primaryKey: true, unique: true, autoIncrement: true},
     chatId: {type: DataTypes.INTEGER, unique:true},
     right: {type: DataTypes.INTEGER, defaultValue: 0},
