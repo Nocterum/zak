0086c9002199a28b9fb26cfb74bf7eaf7417c18f
Nocterum <master.of.colours@yandex.ru>
2023-05-26 13:59:27 +0300

    add save lastCommand & preLastCommand

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ const startGame = async (chatId) => {
         await bot.sendMessage(chatId, `Отгадывай:`, gameOptions)
     }
 
+
 //-------------------------------------------------------------------------------------------------------------------------------------
 
 const start = async () => {
@@ -39,62 +40,92 @@ const start = async () => {
         console.log('Подключение к БД сломалось', err)
     }
 
+    //слушатель сообщений
     bot.on('message', async msg => {
         const text = msg.text;
         const chatId = msg.chat.id;
         console.log(msg)
 
+        const user = await UserModel.findOne({
+            where: {
+                chatId: chatId
+            }
+        });
+
         try {
 
-                try {
-                    if (text === '/start') {
+            try {
+                if (text === '/start') {
+
+                    if (user) {
+                        await user.update(chatId, {preLastCommand: lastCommand});
+                        await user.update(chatId, {lastCommand: '/start'});
+                        return bot.sendMessage(chatId, 
+                            `Привет, ${msg.from.first_name}. Меня зовут бот Зак. 
+                            \nЯ могу подсказать наличие товара по поставщику ОПУС, а так же узнать сроки поставки и запросить резервирование.
+                            \nЧтобы начать работу выбери в меню команду /startwork.`)
+                    } else {
+                        await user.update(chatId, {preLastCommand: lastCommand});
+                        await user.update(chatId, {lastCommand: '/start'});
                         const user = await UserModel.create({chatId});
                         console.log('Новый пользователь создан:', user);
-                        return bot.sendMessage(chatId, `Привет, ${msg.from.first_name}, меня зовут бот Зак. 
-                        \nЯ могу подсказать наличие товара по поставщику ОПУС, а так же узнать сроки поставки и запросить резервирование.
-                        \nЧтобы начать работу выбери в меню команду /startwork.`)
+                        return bot.sendMessage(chatId, 
+                            `Привет, ${msg.from.first_name}. Меня зовут бот Зак.
+                            Приятно познакомиться! Я успешно внёс ваш id"${chatId}" в свою базу данных. 
+                            \nЯ могу подсказать наличие товара по поставщику ОПУС, а так же узнать сроки поставки и запросить резервирование.
+                            \nЧтобы начать работу выбери в меню команду /startwork `)
                     }
-                } catch (e) {
-                    console.log('Ошибка при создании нового пользователя', e);
+                    
                 }
 
-            const user = await UserModel.findOne({
-                where: {
-                    chatId: chatId
-                }
-            });
+            } catch (e) {
+                console.log('Ошибка при создании нового пользователя', e);
+            }
+
     
             if (text === '/info') {
-                return bot.sendMessage(chatId, `Последняя введеная команда "Команда"`)
+                await user.update(chatId, {preLastCommand: lastCommand});
+                await user.update(chatId, {lastCommand: '/info'});
+                return bot.sendMessage(chatId, 
+                    `Последняя введеная команда "Команда"`)
             }
 
             if (text === '/infogame') {
-                
-                return bot.sendMessage(chatId, `Правильных ответов: "${user.right}"
-                                                \nНеправильных ответов: "${user.wrong}"
-                                                \nПоследняя команда: "${user.lastcommand}"
-                                                \nПредпоследняя команда: "${user.preLastcommand}"`, resetOptions);
+                await user.update(chatId, {preLastCommand: lastCommand});
+                await user.update(chatId, {lastCommand: '/infogame'});
+                return bot.sendMessage(chatId, 
+                    `Правильных ответов: "${user.right}"
+                    \nНеправильных ответов: "${user.wrong}"
+                    \nПоследняя команда: "${user.lastCommand}"
+                    \nПредпоследняя команда: "${user.preLastCommand}"`, resetOptions);
                                                 
             }
     
             if (text === '/game') {
-                await bot.sendMessage(chatId, `Сейчас загадаю цифру`)
+                await user.update(chatId, {preLastCommand: lastCommand});
+                await user.update(chatId, {lastCommand: '/game'});
+                await bot.sendMessage(chatId, 
+                    `Сейчас загадаю цифру`)
                 const randomNumber = Math.floor(Math.random() * 10)
                 chats[chatId] = randomNumber;
-                return bot.sendMessage(chatId, `Отгадывай:`, gameOptions)
+                return bot.sendMessage(chatId, 
+                    `Отгадывай:`, gameOptions)
             }
     
         } catch (e) {
-            return bot.sendMessage(chatId, 'Ошибка в исполнении кода', e);
+            return bot.sendMessage(chatId, 
+                'Ошибка в исполнении кода', e);
 
         }
 
-        await bot.sendSticker(chatId, 'https://tlgrm.ru/_/stickers/ccd/a8d/ccda8d5d-d492-4393-8bb7-e33f77c24907/12.webp')
-        return bot.sendMessage(chatId, 'Не понимаю тебя..')
+        await bot.sendSticker(chatId, 
+            'https://tlgrm.ru/_/stickers/ccd/a8d/ccda8d5d-d492-4393-8bb7-e33f77c24907/12.webp')
+        return bot.sendMessage(chatId, 
+            'Не понимаю тебя..')
 
     })
 
-//-------------------------------------------------------------------------------------------------------------------------------------
+//слушатель колбэков-------------------------------------------------------------------------------------------------------------------------------------
 
     bot.on('callback_query', async msg => {
         const data = msg.data;
@@ -108,10 +139,15 @@ const start = async () => {
         })
 
         if (data === '/again') {
+            await user.update(chatId, {preLastCommand: lastCommand});
+            await user.update(chatId, {lastCommand: '/again'});
             return startGame(chatId)
         }
 
         if(data === '/reset') {
+            await user.update(chatId, {preLastCommand: lastCommand});
+            await user.update(chatId, {lastCommand: '/reset'});
+
             if (user) {
                 user.right = 0;
                 user.wrong = 0;
@@ -119,15 +155,20 @@ const start = async () => {
             } else {
                 await UserModel.create({chatId, right: 0, wrong: 0});
             }
-            await bot.sendMessage(chatId, `Результаты игры сброшенны: \nправильных ${user.right}, неправильных ${user.wrong}`, againOptions)
+            return bot.sendMessage(chatId, 
+                `Результаты игры сброшенны: 
+                \nправильных ${user.right}, 
+                \nнеправильных ${user.wrong}`, againOptions)
         }
 
-        if (data == chats[chatId] && user.lastcommand === '/game' || '/again') {
+        if (data == chats[chatId] && user.lastCommand === '/game' || '/again') {
             user.right += 1;
-            return bot.sendMessage(chatId, `Ты отгадал цифру "${chats[chatId]}"`, againOptions)
+            return bot.sendMessage(chatId, 
+                `Ты отгадал цифру "${chats[chatId]}"`, againOptions)
         } else {
             user.wrong += 1;
-            return bot.sendMessage(chatId, `Нет, я загадал цифру "${chats[chatId]}"`, againOptions)
+            return bot.sendMessage(chatId, 
+                `Нет, я загадал цифру "${chats[chatId]}"`, againOptions)
         }
 
     })
