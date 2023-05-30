e5ded68dbdbc107e1ebbcfebe1a6ac34b885fd58
Nocterum <master.of.colours@yandex.ru>
2023-05-30 14:06:04 +0300

    Добавление опций в инфо

--- models.js
+++ models.js
@@ -3,7 +3,7 @@ const {DataTypes} = require('sequelize');
 
 const UserModel = sequelize.define( 'user', {
     id: {type: DataTypes.INTEGER, primaryKey: true, unique: true, autoIncrement: true},
-    chatId: {type: DataTypes.STRING, unique:true},
+    chatId: {type: DataTypes.TEXT, unique:true},
     right: {type: DataTypes.INTEGER, defaultValue: 0},
     wrong: {type: DataTypes.INTEGER, defaultValue: 0},
     firstName: {type: DataTypes.CHAR,},
