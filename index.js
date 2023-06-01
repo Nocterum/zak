537777a4bbf6190d8c5c9ab1e14cf6d387461268
Nocterum <master.of.colours@yandex.ru>
2023-06-01 17:20:00 +0300

    вынес команду game & gameinfo в метод onText. Добавил в игре удаление предыдущего интерфейса после выполнения команды again

--- index.js
+++ index.js
@@ -179,7 +179,7 @@ bot.on('message', async msg => {
                 return bot.sendSticker(chatId, 'https://cdn.tlgrm.app/stickers/087/0cf/0870cf0d-ec03-41e5-b239-0eb164dca72e/192/1.webp')
             }
 
-            if (text != '/game' || '/infogame') {
+            if (text !== '/game' || '/infogame') {
                 await bot.sendSticker(chatId, 'https://tlgrm.ru/_/stickers/ccd/a8d/ccda8d5d-d492-4393-8bb7-e33f77c24907/12.webp')
                 return bot.sendMessage(chatId, 'Не понимаю тебя..')
             }
