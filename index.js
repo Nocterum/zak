a2a19ffc34828caf1cd674c51da2f81e75de65fb
Nocterum <master.of.colours@yandex.ru>
2023-06-02 19:25:58 +0300

    сломанный код

--- index.js
+++ index.js
@@ -70,6 +70,7 @@ const start = async () => {
 
 //старт
 bot.onText(/\/start/, async msg => {
+    123131231231
     const chatId = msg.chat.id;
 
     await bot.deleteMessage(chatId, msg.message_id);
