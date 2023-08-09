9ab87b568e4063878bb6da566e277897a724448d
Nocterum <master.of.colours@yandex.ru>
2023-08-09 15:31:46 +0300

    Бета версия с запросами и емейлами

--- index.js
+++ index.js
@@ -365,7 +365,11 @@ bot.on('message', async msg => {
     //Записываем название бренда в ячейку БД
     if (lc === '/enterReserveNumber') {
         await user.update({reserveNumber: text});
-        return bot.sendMessage(chatId, `Вы желаете зарезервировать партию <b>${text.split(" ")[0]}</b> в колличестве <b>${text.split(" ")[1]}</b> шт? \n<pre>(для перезаписи введите число повторно)</pre>`, enterReserveNumberOptions);
+        if ((user.reserveNumber.split(" ")[0]) !== (user.reserveNumber.split(" ")[1])) {
+            return bot.sendMessage(chatId, `Вы желаете зарезервировать партию <b>${text.split(" ")[0]}</b> в колличестве <b>${text.split(" ")[1]}</b> шт? \n<pre>(для перезаписи введите число повторно)</pre>`, enterReserveNumberOptions);
+        } else {
+            return bot.sendMessage(chatId, `Вы желаете зарезервировать  <b>${uset.vendorCode}</b> в колличестве <b>${text.split(" ")[1]}</b> шт? \n<pre>(для перезаписи введите число повторно)</pre>`, enterReserveNumberOptions);
+        }
     }
 
     //Записываем артикул в ячейку БД и начинаем поиск на сайте
