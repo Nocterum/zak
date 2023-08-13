85df17265b83dcc168b46fa8a10d542fe2740d41
Nocterum <master.of.colours@yandex.ru>
2023-08-13 13:25:52 +0300

    Поиск таблицы на удалёнке 3

--- index.js
+++ index.js
@@ -34,7 +34,6 @@ bot.setMyCommands([
     {command: '/mainmenu', description:'Главное меню'},
     {command: '/beginwork', description:'Начало работы'},
     {command: '/infowork', description:'Проверка введенных параметров'},
-    {command: '/findExcelCatalog', description:'Проверка введенных параметров'},
 ])
 
 
