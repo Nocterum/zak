875fd6bc2ea2645b111e35454685e4dfa2efc828
Nocterum <master.of.colours@yandex.ru>
2023-08-19 23:32:18 +0300

    Поиск в эксель файлах

--- index.js
+++ index.js
@@ -293,7 +293,7 @@ async function findCatalogWallpaper(chatId) {
             const workbook = new ExcelJS.Workbook();
             const stream = fs.createReadStream(filePath);
             const worksheet = await workbook.xlsx.read(stream);
-            const firstWorksheet = worksheet.getWorksheet(8);
+            const firstWorksheet = worksheet.getWorksheet(9);
 
             let foundMatchWallpaper = false;
             let message = '';
