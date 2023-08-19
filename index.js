055f902131cf222e0a51d64b4b9723c62f97a7d5
Nocterum <master.of.colours@yandex.ru>
2023-08-19 19:27:21 +0300

    Поиск в эксель файлах

--- index.js
+++ index.js
@@ -291,7 +291,7 @@ try {
         const stream = fs.createReadStream(fileNameWallpaper);
         await workbookWallpaper.xlsx.read(stream);
         const worksheetWallpaper = await workbookWallpaper.xlsx.read(stream).then(() => {
-            return workbookWallpaper.getWorksheet(14);
+            return workbookWallpaper.getWorksheet(0);
           });
 
         let foundMatchWallpaper = false;
