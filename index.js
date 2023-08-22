b5401fd1ea4ea474c5a101ed244eb92951c52af1
Nocterum <master.of.colours@yandex.ru>
2023-08-22 23:53:35 +0300

    Поиск в эксель файлах

--- index.js
+++ index.js
@@ -283,10 +283,10 @@ async function findCatalogWallpaper(chatId) {
             let message = '';
 
             firstWorksheet.eachRow((row, rowNumber) => {
-                const cellValue = row.getCell('D').value.toString.split("/")[0];   
+                const cellValue = row.getCell('D').value.toString().split("/")[0];   
                 bot.sendMessage (chatId, cellValue);
 
-                if (cellValue === user.catalog) {
+                if (cellValue.includes(user.catalog)) {
                     foundMatchWallpaper = true;
                     const cValue = row.getCell('C').value;
                     const hValue = row.getCell('H').value;
