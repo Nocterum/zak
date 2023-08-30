a6428b7d956bb650afe2d5fb2e54c00758401e8b
Nocterum <master.of.colours@yandex.ru>
2023-08-30 16:51:56 +0300

    Поиск в эксель файлах

--- index.js
+++ index.js
@@ -401,7 +401,7 @@ async function findOrac(chatId) {
                     botMsgIdx = null;
                 }
 
-                return bot.sendMessage(chatId, `На складе в Москве артикул <b>${user.vendorCode}</b> отсутсвует.`,  { parse_mode: "HTML" });
+                await bot.sendMessage(chatId, `На складе в Москве артикул <b>${user.vendorCode}</b> отсутсвует.`,  { parse_mode: "HTML" });
             }
 
         } catch (error) {
@@ -449,7 +449,7 @@ async function findOrac(chatId) {
                     botMsgIdx = null;
                 }
 
-                return bot.sendMessage(chatId, `На складе в Санкт-Петербурге артикул <b>${user.vendorCode}</b> отсутсвует.`, { parse_mode: "HTML" });
+                await bot.sendMessage(chatId, `На складе в Санкт-Петербурге артикул <b>${user.vendorCode}</b> отсутсвует.`, { parse_mode: "HTML" });
             }
 
         } catch (error) {
