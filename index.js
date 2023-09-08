8186db81c19874dfb84f8f968d0092f2d37cf1f5
Nocterum <master.of.colours@yandex.ru>
2023-09-08 22:51:35 +0300

    FTP 1C

--- index.js
+++ index.js
@@ -91,9 +91,9 @@ const startRequest1C = async (chatId) => {
         const updatedResponse = await axios.get(request);
         const updatedDom = new JSDOM(updatedResponse.data);
         const updatedDocument = updatedDom.window.document;
-
+        
         // const tableElement = updatedDocument.querySelector("body > table:nth-child(3)"); // Истинный путь к таблице
-        const tableElement = updatedDocument.querySelector("body > form"); // Истинный путь к таблице
+        const tableElement = updatedDocument.querySelector("body > form > input[type=text]:nth-child(1)"); // Истинный путь к таблице
         
         console.log(tableElement);
         
