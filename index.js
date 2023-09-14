feeb166603aeb514df2e51ed52d8d2a96cc0c2af
Nocterum <master.of.colours@yandex.ru>
2023-09-14 12:47:14 +0300

    FTP 1C

--- index.js
+++ index.js
@@ -94,7 +94,7 @@ const startRequest1C = async (chatId, vendorCode) => {
                     const header1 = cells[0].textContent.trim();
                     const header2 = cells[1].textContent.trim();
                     // const header3 = cells[2].textContent.trim();
-                    console.log(header1, header2, header3);
+                    console.log(header1, header2);
                     return `${header1}, ${header2}`;
                 } else {
                     const cells = row.querySelectorAll('td');
