55a9c0a03c2d87e8aefd27e53c0b5f9c34d49729
Nocterum <master.of.colours@yandex.ru>
2023-09-14 15:08:32 +0300

    FTP 1C

--- index.js
+++ index.js
@@ -119,8 +119,12 @@ const startRequest1C = async (chatId, vendorCode) => {
                     bot.deleteMessage(chatId, botMsgIdx);
                     botMsgIdx = null;
                 }
+                if (obj.warehouse === "Склад") {
+                    return "";
+                } else {
                 const message = formatedData.map(obj => `${obj.warehouse}\nКоличество: ${obj.quantity}; Резерв: ${obj.reserve}\n\n`).join('');
                 return bot.sendMessage(chatId, message);
+                }
             } else {
                 console.log('В таблице нет данных');
             }
