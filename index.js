4c3ec0d536983dabc388c3b818b895e1639f4459
Nocterum <master.of.colours@yandex.ru>
2023-09-28 15:16:33 +0300

    add axios decaro

--- index.js
+++ index.js
@@ -329,6 +329,7 @@ const startFindDecaro = async (chatId) => {
             console.log('успешно зашёл на страницу товара');
 
             const inner_props = $$('div.inner_props div.prop');
+            const availabilityTable = $$('div.availability-table')
             let chars = '';
 
             // создаем массив объектов с данными из каждого элемента prop
@@ -345,6 +346,67 @@ const startFindDecaro = async (chatId) => {
             propsData.forEach((item) => {
                 chars += `${item.name}: ${item.value}\n`;
             });
+
+            if (botMsgIdx !== null) {
+                bot.deleteMessage(chatId, botMsgIdx);
+                botMsgIdx = null;
+            }
+
+            await bot.sendMessage(
+                chatId,
+                chars,
+                { parse_mode: "HTML" }
+            );
+
+            await bot.sendMessage(
+                chatId,
+                `Ожидаю ответ от сервера по остаткам . . .`,
+                { parse_mode: "HTML" }
+            );
+            botMsgIdx = msg.message_id += 1; 
+
+            const availabilityTableValue = availabilityTable.map((index, element) => {
+                const rowsStatus = $$(element).find('div.status');
+                const rowsDays = $$(element).find('div.days');
+                const rowsArticul = $$(element).find('div.articul');
+                const rowsQty = $$(element).find('div.qty');
+                const rowsUnit = $$(element).find('div.unit');
+                const rowsOther = $$(element).find('small');
+                
+                return {
+                    status: rowsStatus.text().trim(),
+                    days: rowsDays.text().trim(),
+                    articul: rowsArticul.text().trim(),
+                    qty: rowsQty.text().trim(),
+                    unit: rowsUnit.text().trim(),
+                    other: rowsOther.text().trim()
+                }
+            }).get(); // преобразуем объект Cheerio в обычный массив
+
+            chars = '';
+
+            // выводим данные из каждого элемента массива propsData
+            availabilityTableValue.forEach((item) => {
+                chars += `<b>${item.status}: </b>`;
+                if (item.days !== null && item.days !== undefined) {
+                    chars += `${item.days}`;
+                }
+                if (item.articul !== null && item.articul !== undefined) {
+                    chars += `${item.articul} `;
+                }
+                if (item.qty !== null && item.qty !== undefined) {
+                    chars += `${item.qty} `;
+                }
+                if (item.unit !== null && item.unit !== undefined) {
+                    chars += `${item.unit}\n`;
+                }
+                chars += `${item.other}\n`
+            });
+
+            if (botMsgIdx !== null) {
+                bot.deleteMessage(chatId, botMsgIdx);
+                botMsgIdx = null;
+            }
 
             return bot.sendMessage(
                 chatId,
