b87a67912e274b5556be16c8de32fdec9a6da758
Nocterum <master.of.colours@yandex.ru>
2023-10-03 08:49:01 +0300

    add axios decaro

--- index.js
+++ index.js
@@ -514,8 +514,8 @@ const startFindLevantin = async (chatId, msg) => {
 
             // Извлекаем нужные строки из Блока характеристик 1
             const charsBlock1 = $$('.small-12.medium-6.large-8.columns.catalog-detail__text .row .small-12.columns');
-            const charsBlock2 = charsBlock1.find('.row').eq(0);
-            const charsBlock3 = charsBlock1.find('.row').eq(1);
+            const charsBlock2 = charsBlock1.find('.row').eq(1);
+            const charsBlock3 = charsBlock1.find('.row').eq(2);
 
             charsBlock1.each((index, element) => {
                 const row = $(element).text().trim().replace(/\s+/g, ' ').replace(/\n+/g, '\n'); // Получаем текст строки и удаляем лишние пробелы
