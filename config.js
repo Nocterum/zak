532e92524c1b380ec3bfafaf430083ddaa602f92
Nocterum <master.of.colours@yandex.ru>
2023-10-09 16:33:20 +0300

    add axios decaro

--- config.js
+++ config.js
@@ -6,28 +6,28 @@
 //     config[key] = value;
 // });
 
-const util = require('util');
-const readFile = util.promisify(fs.readFile);
+// const util = require('util');
+// const readFile = util.promisify(fs.readFile);
 
-async function readConfig() {
+// async function readConfig() {
     
-    try {
-        const data = await readFile('/root/zak/config.cfg', 'utf-8');
-        const lines = data.split('\n');
-    const config = {};
+//     try {
+//         const data = await readFile('/root/zak/config.cfg', 'utf-8');
+//         const lines = data.split('\n');
+//     const config = {};
     
-    lines.forEach(line => {
-        const [key, value] = line.trim().split('=');
-        config[key] = value;
-    });
+//     lines.forEach(line => {
+//         const [key, value] = line.trim().split('=');
+//         config[key] = value;
+//     });
     
-    return config;
-} catch (error) {
-    console.error('Ошибка при чтении файла конфигурации:', error);
-    throw error;
-}
-}
+//     return config;
+// } catch (error) {
+//     console.error('Ошибка при чтении файла конфигурации:', error);
+//     throw error;
+// }
+// }
 
 
 
-module.exports = readConfig();
+// module.exports = readConfig();
